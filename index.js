b8bdbdc8ea9cfdfbffa0b5b5bdba886e4f6f9d32
Angel Hernandez <angel@yogome.com>
2017-11-23 21:03:40 -0600

    Server iniciado

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var routes = require("./app/index.js")(app);
+var routes = require("./app/app.js")(app);
 
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+    console.log("Listening on port %s...");
 });
